a8818e2cca6060ae46a73d43c22d17ade6b39461
Gonzalo <gonzalo.ie.ucr@gmail.com>
2025-12-02 08:38:39 -0600

    Fix

--- examples/stm32/f4/stm32f429i-discovery/crc-example/monitor-crc.c
+++ examples/stm32/f4/stm32f429i-discovery/crc-example/monitor-crc.c
@@ -70,7 +70,7 @@ static uint32_t crc32_hw_bytes(const uint8_t *data, size_t len)
 /*********** CLOCKS ***********/
 static void clock_setup(void)
 {
-    rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+    rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOF);
